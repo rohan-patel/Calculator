78a3b9b80736fdd552035e090a0e8a326547dbf6
Rohan Patel <rohanpatel1531@gmail.com>
2021-09-05 00:55:42 +0530

    Bug Fixes

--- script.js
+++ script.js
@@ -88,8 +88,9 @@ class Calculator {
     this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
     if (this.operation != null) {
       this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    } else {
+      this.previousOperandTextElement.innerText = ''
     }
-    
   }
 }
 
